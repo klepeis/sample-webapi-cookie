63b878b1821a66e7fcdc12c52e50738b9ee13c28
Matt Klepeis <mattklepeis@gmail.com>
2020-06-26 10:35:18 -0400

    Cookie Controller

---
+++ src/Demo.Api/Controllers/CookieController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Demo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CookieController : ControllerBase
+    {
+        [HttpPost]
+        [HttpGet("add")]
+        public IActionResult SetCookie()
+        {
+            CookieOptions options = new CookieOptions();
+            options.HttpOnly = true;
+            options.Secure = true;
+            options.Expires = DateTimeOffset.Now.AddDays(60);
+
+            Response.Cookies.Append("uniqueId", "cookieValue", options);
+
+            return Ok("Cookie Value Set");
+        }
+
+        [HttpGet]
+        public IActionResult GetCookie()
+        {
+            string value = Request.Cookies["uniqueId"];
+            return Ok($"The value is {value}.");
+        }
+
+        [HttpDelete]
+        [HttpGet("delete")]
+        public IActionResult DeleteCookie()
+        {
+            Response.Cookies.Delete("uniqueId");
+            return Ok("Cookie Deleted");
+        }
+    }
+}
--- src/Demo.Api/Startup.cs
+++ src/Demo.Api/Startup.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Demo.Api
 {
@@ -27,19 +18,6 @@ namespace Demo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = "Cookies";
-            }).AddCookie("Cookies", options =>
-            {
-                options.Cookie.Name = "auth_cookie";
-                options.Cookie.SameSite = SameSiteMode.None;
-                options.Events = new CookieAuthenticationEvents 
-                {
-                    a
-                }
-            });
-
             services.AddControllers();
         }
 
